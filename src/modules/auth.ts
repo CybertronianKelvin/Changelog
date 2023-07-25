03024afa8632ada5a26baa28a0a6e2fd052da80f
kelvinatawurausay <kelvin.atawura@theteachercloud.net>
2023-07-25 21:16:15 +0100

    Jwt implementation with Primsa

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,4 +1,16 @@
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+export const comparePasswords = (
+  password: string,
+  hashedPassword
+): Promise<boolean> => {
+  return bcrypt.compare(password, hashedPassword);
+};
+
+export const hashPassword = (password: string): Promise<string> => {
+  return bcrypt.hash(password, 5);
+};
 
 export const createJWT = (user) => {
   const token = jwt.sign(
@@ -6,4 +18,35 @@ export const createJWT = (user) => {
     process.env.JWT_SECRET
   );
   return token;
+};
+
+export const protect = (req, res, next) => {
+  const bearer = req.headers.authorization;
+
+  if (!bearer) {
+    res.status(401);
+    res.json({ message: "not authorized" });
+    return;
+  }
+
+  const [, token] = bearer.split(" ");
+
+  if (!token) {
+    res.status(401);
+    res.json({ message: "not valid token" });
+    return;
+  }
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = payload;
+    console.log(payload);
+    next();
+    return;
+  } catch (e) {
+    console.error(e);
+    res.status(401);
+    res.send("Not authorized");
+    return;
+  }
 };
